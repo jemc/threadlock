5ad691f8014235461c62f1c35d256472d7b4f656
Joe McIlvain <joe.eli.mac@gmail.com>
2013-09-24 22:34:17 -0400

    Revised strategy to capture method object, use define_method, and bind within.

--- lib/threadlock.rb
+++ lib/threadlock.rb
@@ -1,35 +1,14 @@
 require 'monitor'
 
-def threadlock(*funcs, lock: :@___threadlock___)
-  for f in funcs.flatten
-    f2 = f.to_s
-          .gsub(/\=/, "_eQUAL")
-          .gsub(/\!/, "_nOT")
-          .gsub(/\~/, "_tILDE")
-          .gsub(/\</, "_lESS")
-          .gsub(/\>/, "_mORE")
-          .gsub(/\+/, "_aDD")
-          .gsub(/\-/, "_sUB")
-          .gsub(/\*/, "_mULT")
-          .gsub(/\//, "_dIV")
-          .gsub(/\%/, "_mOD")
-          .gsub(/\[/, "_bRACKO")
-          .gsub(/\]/, "_bRACKC")
-          .gsub(/\@/, "_aTTR")
-          .gsub(/\&/, "_aMPER")
-          .gsub(/\|/, "_pIPE")
-          .gsub(/\^/, "_cARET")
-          .gsub(/^/, "___")
-    
-    class_eval\
-    ("
-      alias :#{f2} :#{f}
-      def #{f}(*args, &block)
-        #{lock.to_s} ||= Monitor.new
-        #{lock.to_s}.synchronize do
-          #{f2}(*args, &block)
-        end
+def threadlock(*meths, lock: :@___threadlock___)
+  meths.each do |meth|
+    m = instance_method(meth)
+    define_method(meth) do |*args|
+      (instance_variable_get(lock) or \
+       instance_variable_set(lock, Monitor.new))
+      .synchronize do
+        m.bind(self).call(*args)
       end
-    ")
+    end
   end
 end
